3f3dc86dbde27170894de16be47f822ed4e9731f
Irina <irabreus@gmail.com>
2021-01-29 13:26:01 +0300

    finaly

---
+++ src/js/modules/download.js
@@ -0,0 +1,30 @@
+export default class Download {
+    constructor(triggers) {
+        this.btns = document.querySelectorAll(triggers);
+        this.path = 'assets/img/mainbg.jpg';
+    }
+
+    downloadItem(path) {
+        const link = document.createElement('a');
+
+        link.setAttribute('href', path);
+        link.setAttribute('download', 'my_name');
+
+        link.style.display = 'none';
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+    }
+
+    init() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                this.downloadItem(this.path);
+            });
+        });
+    }
+}
